4486ceb4c11e499e3850fe2cd43145c8997d6953
dongmingyan <dongmingyan01@gmail.com>
2023-07-30 10:26:41 +0800

    JSON 解析和验证器封装

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -2,6 +2,9 @@
 package requests
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/thedevsaddam/govalidator"
 )
@@ -10,7 +13,7 @@ type SignupPhoneExistRequest struct {
 	Phone string `json:"phone,omitempty" valid:"phone"`
 }
 
-func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
+func SignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
 
 	// 自定义验证规则
 	rules := govalidator.MapData{
@@ -25,23 +28,14 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 		},
 	}
 
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
 
 type SignupEmailExistRequest struct {
 	Email string `json:"email,omitempty" valid:"email"`
 }
 
-func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
+func SignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
 
 	// 自定义验证规则
 	rules := govalidator.MapData{
@@ -58,14 +52,53 @@ func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]str
 		},
 	}
 
-	// 配置初始化
+	// 开始验证
+	return validate(data, rules, messages)
+}
+
+// ValidatorFunc 验证函数类型
+type ValidatorFunc func(interface{}, *gin.Context) map[string][]string
+
+// Validate 控制器里调用示例：
+//
+//	if ok := requests.Validate(c, &requests.UserSaveRequest{}, requests.UserSave); !ok {
+//	    return
+//	}
+func Validate(c *gin.Context, obj interface{}, handler ValidatorFunc) bool {
+
+	// 1. 解析请求，支持 JSON 数据、表单请求和 URL Query
+	if err := c.ShouldBind(obj); err != nil {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"message": "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。",
+			"error":   err.Error(),
+		})
+		fmt.Println(err.Error())
+		return false
+	}
+
+	// 2. 表单验证
+	errs := handler(obj, c)
+
+	// 3. 判断验证是否通过
+	if len(errs) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"message": "请求验证不通过，具体请查看 errors",
+			"errors":  errs,
+		})
+		return false
+	}
+
+	return true
+}
+
+func validate(data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
+	// 配置选项
 	opts := govalidator.Options{
 		Data:          data,
 		Rules:         rules,
 		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
 		Messages:      messages,
 	}
-
 	// 开始验证
 	return govalidator.New(opts).ValidateStruct()
 }
