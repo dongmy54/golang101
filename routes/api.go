1fed30bee611d00952ca643039e11376c4d9144d
dongmingyan <dongmingyan01@gmail.com>
2023-08-05 11:50:24 +0800

    手机 + 短信验证码登录

--- routes/api.go
+++ routes/api.go
@@ -23,13 +23,16 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			// 发送验证码
 			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
 			authGroup.POST("/signup/using-email", suc.SignupUsingEmail)
+
+			vcc := new(auth.VerifyCodeController)
+			// 图片验证码，需要加限流
+			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
+			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
+			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
+
+			lgc := new(auth.LoginController)
+			// 使用手机号，短信验证码进行登录
+			authGroup.POST("/login/using-phone", lgc.LoginByPhone)
 		}
-
-		vcc := new(auth.VerifyCodeController)
-		// 图片验证码，需要加限流
-		authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
-		authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
-		authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
-
 	}
 }
